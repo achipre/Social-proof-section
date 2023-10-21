2189281c8ce4cdbba13a335d2d3f364ad2e0c4d5
Alex Chipre <alechipre@gmail.com>
2023-10-21 12:17:28 -0500

    Add responsive model

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Irene from './assets/image-irene.jpg'
 import Anne from './assets/image-anne.jpg'
 export const App = () => {
   return (
-    <main className="flex flex-col items-center gap-4 max-w-[1120px] m-auto">
-      <div className="flex items-center flex-wrap w-full justify-center">
+    <main className="flex flex-col items-center gap-4 sm:gap-12 md:gap-[70px] max-w-[1126px] m-auto">
+      <div className="flex items-center flex-wrap w-full justify-center lg:justify-between sm:mx-4">
         <aside className="flex flex-col gap-4 w-full items-center md:items-start max-w-[412px]">
           <h1 className="text-very-dark-magenta text-4xl font-bold text-center leading-7 mt-6 max-w-[312px] sm:max-w-[412px] sm:text-[3.25rem] sm:leading-[3.12rem] sm:text-start">
             10,000+ of our users love our products.
           </h1>
-          <p className="text-[.9375rem] text-center text-very-dark-magenta mt-2 max-w-[412px] sm:text-lg sm:text-start sm:leading-[1.6rem]">
+          <p className="text-[.9375rem] text-center text-dark-grayish-magenta mt-2 max-w-[412px] sm:text-lg sm:text-start sm:leading-[1.6rem]">
             We only provide great products combined with excellent customer service. See what our
             satisfied customers are saying about our services.
           </p>
@@ -56,44 +56,44 @@ export const App = () => {
         </aside>
       </div>
 
-      <div className="flex justify-center gap-4 md:gap-8 flex-wrap">
-        <article className="bg-very-dark-magenta text-white rounded-lg p-6 text-sm flex flex-col gap-6 mt-10 sm:mt-0 max-w-[352px] sm:p-[31px] sm:py-10 lg:max-h-[236px]">
+      <div className="flex justify-center gap-[12px] md:gap-[30px] flex-wrap">
+        <article className="bg-very-dark-magenta text-white rounded-lg p-6 text-sm flex flex-col gap-6 mt-10 sm:mt-0 max-w-[350px] sm:p-8 sm:py-10 lg:max-h-[236px] justify-between">
           <div className="flex gap-4 items-center sm:gap-6">
             <img className="rounded-full w-10" src={Colton} alt="Avatar of Colton" />
-            <div>
-              <p className="sm:text-[1.135rem]">Colton Smith</p>
-              <p className="text-soft-pink sm:text-[1.05rem]">Verified Buyer</p>
+            <div className='flex flex-col gap-1'>
+              <p className="sm:text-[1.1rem] leading-4">Colton Smith</p>
+              <p className="text-soft-pink sm:text-[1.05rem] leading-4">Verified Buyer</p>
             </div>
           </div>
-          <p className="text-[.933rem] sm:text-[1.05rem] sm:leading-[22px]">
-            "We needed the same printed design as the one we had ordered a week prior. Not only did
-            they find the original order, but we also received it in time. Excellent!"
+          <p className="text-[.933rem] sm:text-[1.04rem] sm:leading-[22px] text-light-grayish-magenta">
+            " We needed the same printed design as the one we had ordered a week prior. Not only did
+            they find the original order, but we also received it in time. Excellent! "
           </p>
         </article>
-        <article className="bg-very-dark-magenta text-white rounded-md p-6 text-sm flex flex-col gap-6 max-w-[352px] sm:p-[31px] sm:py-10 lg:max-h-[236px] lg:mt-4">
+        <article className="bg-very-dark-magenta text-white rounded-lg p-6 text-sm flex flex-col gap-6 max-w-[350px] sm:p-[31px] sm:py-10 lg:max-h-[236px] lg:mt-4 justify-between">
           <div className="flex gap-4 items-center sm:gap-6">
             <img className="rounded-full w-10" src={Irene} alt="Avatar of Irene" />
-            <div>
-              <p className="sm:text-[1.135rem]">Irene Roberts</p>
-              <p className="text-soft-pink sm:text-[1.05rem]">Verified Buyer</p>
+            <div className='flex flex-col justify-between'>
+              <p className="sm:text-[1.1rem] leading-5">Irene Roberts</p>
+              <p className="text-soft-pink sm:text-[1.05rem] leading-5">Verified Buyer</p>
             </div>
           </div>
-          <p className="text-[.933rem] sm:text-[1.05rem] sm:leading-[22px]">
-            "Customer service is always excellent and very quick turn around. Completely delighted
+          <p className="text-[.933rem] sm:text-[1.04rem] sm:leading-[22px] sm:pr-2">
+            " Customer service is always excellent and very quick turn around. Completely delighted
             with the simplicity of the purchase and the speed of delivery."
           </p>
         </article>
-        <article className="bg-very-dark-magenta text-white rounded-md p-6 text-sm flex flex-col gap-6 mb-12 sm:mb-0 max-w-[352px] sm:p-[31px] sm:py-10 lg:max-h-[236px] lg:mt-8">
+        <article className="bg-very-dark-magenta text-white rounded-lg p-6 text-sm flex flex-col gap-6 mb-12 sm:mb-0 max-w-[350px] sm:p-[31px] sm:py-10 lg:max-h-[236px] lg:mt-8 justify-between">
           <div className="flex gap-4 items-center sm:gap-6">
             <img className="rounded-full w-10" src={Anne} alt="Avatar of Anne" />
             <div>
-              <p className="sm:text-[1.135rem]">Anne Wallace</p>
+              <p className="sm:text-[1.1rem]">Anne Wallace</p>
               <p className="text-soft-pink sm:text-[1.05rem]">Verified Buyer</p>
             </div>
           </div>
-          <p className="text-[.933rem] sm:text-[1.05rem] sm:leading-[22px]">
-            "Put an order with this company and can only praise them for the very high standard.
-            Will definitely use them again and recommend them to everyone!"
+          <p className="text-[.933rem] sm:text-[1.05rem] sm:leading-[22px]  text-light-grayish-magenta pt-1">
+            " Put an order with this company and can only praise them for the very high standard.
+            Will definitely use them again and recommend them to everyone! "
           </p>
         </article>
       </div>
